44bb547ac77870678de7bd5f3998ee9a23410b2d
4-alokk <alokkumar.business@gmail.com>
2024-10-28 12:24:30 +0530

    cors

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 const express = require('express')
+const cors = require('cors');
+
+const corsOptions ={
+  origin:'http://localhost:3000', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
+
 const app = express()
 const port = 3000
 
-// app.use('/user/:id', (req, res, next) => {
-//   console.log("Request URL:", req.originalUrl)
-//   next()
-// })
-
-// app.use('/user/:id', (req, res, next) => {
-//   console.log('Request URL:', req.originalUrl)
-//   next()
-// }, (req, res, next) => {
-//   console.log('I am the second middleware')
-//   next()
-// })
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('root')
@@ -24,7 +21,7 @@ app.get('/user', (req, res) => {
   let id = 10;
   let message = "Hello World";
   res.json({message, id});
-  
+
 
 })
 
